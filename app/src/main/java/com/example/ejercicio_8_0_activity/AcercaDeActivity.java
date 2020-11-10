f891222b7bf4691016db69a96a87b66fa024b96d
Oier <oz.irakasle@gmail.com>
2020-11-10 08:36:45 +0100

    proba

---
+++ app/src/main/java/com/example/ejercicio_8_0_activity/AcercaDeActivity.java
@@ -0,0 +1,16 @@
+package com.example.ejercicio_8_0_activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AcercaDeActivity extends AppCompatActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.acercade);
+		System.out.println();
+
+	}
+}
